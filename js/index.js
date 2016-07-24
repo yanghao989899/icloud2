6c1c924e5b4c4e95a1f5ae6bfe44b496c3a4c8c9
yanghao989899 <1564559542@qq.com>
2016-07-24 19:04:05 +0800

    5
    5

---
+++ js/index.js
@@ -0,0 +1,137 @@
+/*
+ 	todo={
+		id:1,
+		title:"新列表",
+		time:"",
+		status:"false",
+		list:{
+			con:"",
+			
+		}
+
+ 	}
+
+
+
+*/
+todo=[{
+	id:1,
+	title:"新列表",
+	color:"#C96FE2",
+	list:[{
+			con:"4891565",
+			time:"46516156",
+			status:false
+		},{
+			con:"4891565",
+			time:"46516156",
+			status:false
+		},{
+			con:"4891565",
+			time:"46516156",
+			status:true
+		},{
+			con:"4891565",
+			time:"46516156",
+			status:false
+		}
+	]
+},{
+	id:2,
+	title:"新列表2",
+	color:"#6DDB30",
+	list:[{
+			con:"48161556165",
+			time:"46516156",
+			status:true
+		},{
+			con:"4891565",
+			time:"46516156",
+			status:false
+		},{
+			con:"4891565",
+			time:"46516156",
+			status:true
+		},{
+			con:"4891565",
+			time:"46516156",
+			status:true
+		}
+	]
+}
+]
+var color=["#C96FE2","#6DDB30","#40ABF8","#F2CB00","#9F855D","#F8246A","#F89600"];
+var app=angular.module("app",[]);
+app.controller("icloud",function($scope){
+	$scope.todo=todo;
+	$scope.color=color;
+	$scope.index=0;
+	$scope.add=function(){
+		$scope.ids=$scope.todo[$scope.todo.length-1].id+1;
+		$scope.todo.push({
+			id:$scope.ids,
+			color:color[$scope.ids%7],
+			title:"新列表"+$scope.ids,
+			list:[]
+		});
+	}
+	getnum();
+	function getnum(){
+		$scope.comNum=0;
+		angular.forEach(todo[$scope.index].list,function(o,i){
+			if(o.status==true){
+				$scope.comNum++;
+			}
+		})
+	}
+	$scope.flag=false;
+	$scope.show=function(){
+		$scope.flag=!$scope.flag;
+	}
+	$scope.com=function(val,index,arr){
+		return val.status==true?true:false;
+	}
+	$scope.doing=function(val,index,arr){
+		return val.status==false?true:false;
+	}
+	$scope.select=function(i){
+		$scope.index=i;
+		getnum();
+		$scope.flag=false;
+		getColor();
+	}
+	$scope.new=function(){
+		$scope.todo[$scope.index].list.push({
+			con:"",
+			time:new Date().getTime(),
+			status:false
+		})
+	}
+	$scope.f=false;
+	$scope.change=function(obj,sta){
+		obj.status=sta;
+		getnum();
+	}
+	$scope.fontColor=$scope.todo[$scope.index].color;
+	function getColor(){
+		$scope.fontColor=$scope.todo[$scope.index].color;
+	}
+	$scope.changeColor=function(v){
+		$scope.fontColor=v;
+	}
+	$scope.finish=function(){}
+	$scope.del=function(){
+		if($scope.todo.length>1){
+			$scope.todo.splice($scope.index,1);
+			$scope.f=false;
+		}else{
+			alert("无法删除");
+		}
+	}
+	$scope.titles=$scope.todo[$scope.index].title;
+	$scope.finish=function(){
+		$scope.todo[$scope.index].title=$scope.titles;
+		$scope.todo[$scope.index].color=$scope.fontColor;
+		$scope.f=false;
+	}
+})
